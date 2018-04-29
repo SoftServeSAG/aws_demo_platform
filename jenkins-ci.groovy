556b127ab073c7a35d4ba3203936c707bfe157d2
Iurii Milovanov <duruku@gmail.com>
2018-04-29 17:20:11 -0500

    Added customer_product_analytics app; Disabled a lookup for a sample dataset in S3

--- jenkins-ci.groovy
+++ jenkins-ci.groovy
@@ -1,16 +1,16 @@
 node {
     def github_user = "SoftServeSAG"
-    def app_names = ["aws_anomaly_detection", "scorecard_builder_app"]
-    def s3_bucket = "softserve-analytics"
+    def app_names = ["aws_anomaly_detection", "scorecard_builder_app", "customer_product_analytics"]
+    // def s3_bucket = "softserve-analytics"
     def app_port = 3340
     stage('Extract') {
         parallel app_names.collectEntries {app -> [app, {
             node {
                 dir("sources/${app}") {
                     git url: "https://github.com/${github_user}/${app}"
-                    sh "wget -q https://s3.amazonaws.com/${s3_bucket}/${app}.zip"
-                    sh "unzip -o ${app}.zip"
-                    sh "rm ${app}.zip"
+                    // sh "wget -q https://s3.amazonaws.com/${s3_bucket}/${app}.zip"
+                    // sh "unzip -o ${app}.zip"
+                    // sh "rm ${app}.zip"
                 }
             }
         }]}
